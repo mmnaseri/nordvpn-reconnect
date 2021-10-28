4e82bd03da8008fd8dba1d94f7eccca48be9feed
Milad Naseri <m.m.naseri@gmail.com>
2021-10-27 18:44:53 -0700

    Update service definition and installer

--- install.sh
+++ install.sh
@@ -6,5 +6,11 @@ cd "$(dirname "${0}")" || exit 1
 
 ./uninstall.sh
 
+echo "- Creating service definition and binary"
 ln -s "$(pwd)/nordvpn-reconnect.sh" "/usr/bin/nordvpn-reconnector"
 cp "$(pwd)/nordvpn-reconnector.service" "/etc/systemd/system/nordvpn-reconnector.service"
+
+echo
+echo "Do not forget to enable and run this service:"
+echo "systemctl enable nordvpn-reconnector.service"
+echo "systemctl start nordvpn-reconnector.service"
